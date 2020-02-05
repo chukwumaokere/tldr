09b0f49a55c1154a273d3338ed5a4f8163dd9a96
Nathan <nathdennis@googlemail.com>
2020-02-05 00:14:25 +0000

    pushing ArticlesController for debugging

--- api/controllers/ArticlesController.js
+++ api/controllers/ArticlesController.js
@@ -6,75 +6,84 @@
  */
 
 module.exports = {
-  getHomePage: async function (req, res){
+  getHomePage: async function(req, res) {
     const query = `SELECT articlename, rating FROM articles WHERE public = 1`;
-    const result = await sails.sendNativeQuery(query)    
-    let articles = []    
+    const result = await sails.sendNativeQuery(query);
+    let articles = [];
 
-    if(result.rows.length > 0){
-      result.rows.forEach((article) => {
+    if (result.rows.length > 0) {
+      result.rows.forEach(article => {
         articles.push({
           title: article.articlename,
           rating: article.rating
-        })
-      })
+        });
+      });
     } else {
-      return res.notFound()
+      return res.notFound();
     }
 
-    return res.view('pages/homepage', { articles })
+    return res.view("pages/homepage", { articles });
   },
 
-  getPage: async function(req, res){
-    var articlename = req.param('articlename').toLowerCase();
+  getPage: async function(req, res) {
+    var articlename = req.param("articlename").toLowerCase();
     var query = `SELECT * FROM articles WHERE articlename = $1 AND public = 1`;
-    var rawResult = await sails.sendNativeQuery(query, [articlename]); 
+    var rawResult = await sails.sendNativeQuery(query, [articlename]);
 
-    if(rawResult.rows.length > 0){ 
-        var target = rawResult.rows[0];
-        var content = JSON.parse(target.content);
-        var articlename_returned = target.articlename;
-        const rating = target.rating
-        var createdTime = new Date(target.createdAt);
-        var updatedTime = new Date(target.updatedAt);
-        // sails.log(articlename_returned, content, createdTime, updatedTime);
-        return res.view('pages/article', {article: content, articlename: articlename_returned, rating, ct: createdTime, ut: updatedTime})
-    }else{
-        return res.notFound(); //If page doesnt exist, give them a 404. TODO: Later we can add a suggestion page to intake suggestions or fully fleshed articles.
+    if (rawResult.rows.length > 0) {
+      var target = rawResult.rows[0];
+      var content = JSON.parse(target.content);
+      var articlename_returned = target.articlename;
+      const rating = target.rating;
+      var createdTime = new Date(target.createdAt);
+      var updatedTime = new Date(target.updatedAt);
+      // sails.log(articlename_returned, content, createdTime, updatedTime);
+      return res.view("pages/article", {
+        article: content,
+        articlename: articlename_returned,
+        rating,
+        ct: createdTime,
+        ut: updatedTime
+      });
+    } else {
+      return res.notFound(); //If page doesnt exist, give them a 404. TODO: Later we can add a suggestion page to intake suggestions or fully fleshed articles.
     }
-  },    
-  getArticles: async function(req, res){
+  },
+  getArticles: async function(req, res) {
     var query = `SELECT * FROM articles`;
     var rawResult = await sails.sendNativeQuery(query, []);
-    if(rawResult.rows.length > 0){
-        var data = rawResult.rows;
-        return res.ok(data);
-    }else{
-        return res.ok([]);
+    if (rawResult.rows.length > 0) {
+      var data = rawResult.rows;
+      return res.ok(data);
+    } else {
+      return res.ok([]);
     }
   },
-  getArticle: async function(req, res){
-    var articlename = req.param('articlename').toLowerCase();
+  getArticle: async function(req, res) {
+    var articlename = req.param("articlename").toLowerCase();
     var query = `SELECT * FROM articles WHERE articlename = $1`;
-    var rawResult = await sails.sendNativeQuery(query, [articlename]); 
+    var rawResult = await sails.sendNativeQuery(query, [articlename]);
 
-    if(rawResult.rows.length > 0){ 
-        var data = rawResult.rows[0];   
-        return res.ok(data);
-    }else{
-        return res.ok([]); //If page doesnt exist, give them a 404. TODO: Later we can add a suggestion page to intake suggestions or fully fleshed articles.
+    if (rawResult.rows.length > 0) {
+      var data = rawResult.rows[0];
+      return res.ok(data);
+    } else {
+      return res.ok([]); //If page doesnt exist, give them a 404. TODO: Later we can add a suggestion page to intake suggestions or fully fleshed articles.
     }
   },
-  generateArticle: async function(req, res){
-    const newArticleName = req.body.articlename
-    const checkArticlesQuery = `SELECT articlename FROM articles WHERE articlename = $1`
-    const checkArticlesResult = await sails.sendNativeQuery(checkArticlesQuery, [newArticleName])
-    const articleExists = checkArticlesResult.rows.length > 0 ? true : false    
-    const createdAt = Math.round(new Date().getTime())
-    const updatedAt = Math.round(new Date().getTime())
-    
-    if (articleExists){
-      res.badRequest('Article already exists')
+  generateArticle: async function(req, res) {
+    const newArticleName = req.body.articlename;
+    const checkArticlesQuery = `SELECT articlename FROM articles WHERE articlename = $1`;
+    const checkArticlesResult = await sails.sendNativeQuery(
+      checkArticlesQuery,
+      [newArticleName]
+    );
+    const articleExists = checkArticlesResult.rows.length > 0 ? true : false;
+    const createdAt = Math.round(new Date().getTime());
+    const updatedAt = Math.round(new Date().getTime());
+
+    if (articleExists) {
+      res.badRequest("Article already exists");
     }
 
     const insertArticleQuery = `INSERT INTO articles (
@@ -85,7 +94,7 @@ module.exports = {
       updatedAt,
       public,
       rating)
-      VALUES($1, $2, $3, $4, $5, $6, 0)`
+      VALUES($1, $2, $3, $4, $5, $6, 0)`;
 
     await sails.sendNativeQuery(insertArticleQuery, [
       newArticleName,
@@ -93,76 +102,110 @@ module.exports = {
       req.body.sources,
       createdAt,
       updatedAt,
-      req.body.public])
+      req.body.public
+    ]);
 
-    return res.ok('Article created')
+    return res.ok("Article created");
   },
-  updateArticle: async function(req, res){
-    const articleName = req.param('articlename').toLowerCase()
+  updateArticle: async function(req, res) {
+    // CURRENT ARTICLE
+    const articleName = req.body.articlename
+    const getCurrentArticleQuery = `SELECT * FROM articles WHERE articlename = $1`
+    const queryResult = await sails.sendNativeQuery(getCurrentArticleQuery, [articleName])
+    const articleProperties = queryResult.rows[0]
+
+    const oldContent = JSON.parse(articleProperties.content)
+    const oldSources = JSON.parse(articleProperties.sources)
+    const newContent = req.body.newContent
+    const newSources = req.body.newSources
+
+    const appendedContent = JSON.stringify(oldContent.concat(newContent))
+    const appendedSources = JSON.stringify(oldSources.concat(newSources))
+
+    console.log(typeof newContent)
+    console.log(newContent)
+    console.log(typeof newSources)
+    console.log(newSources)
+    console.log(typeof appendedContent)
+    console.log(appendedContent)
+    console.log(typeof appendedSources)
+    console.log(appendedSources)
+
+    console.log(`UPDATE articles SET content = ${appendedContent}, sources = ${appendedSources} WHERE articlename = ${articleName}`)
+
+    const articleExists = queryResult.rows.length > 0 ? true : false
     const updateMethod = req.body.updateMethod
-    const contentToUpdate = req.body.newContent
-    const sourcesToUpdate = req.body.newSources
     const updatedPublicStatus = req.body.public
-    const updatedAtTime = Math.round(new Date().getTime())
+    const updatedAt = Math.round(new Date().getTime())
 
-    const articleExists = await sails.sendNativeQuery(`SELECT * FROM articles WHERE articlename = $1`, [articleName])
-    const currentContent = currentArticleBody.content
-    const currentSources = currentArticleBody.sources
+    // if (!articleExists) {
+    //   return res.badRequest("No article found with that name. Please create the article first, or try again with an existing article name")
+    // }
 
-    const newContentArray = currentContent.concat(contentToUpdate)
-    const newSourcesArray = currentSources.concat(sourcesToUpdate)
+    // if (updateMethod === undefined) {
+    //   return res.badRequest("updateMethod must be defined. Options are 'append' & 'overwrite'")
+    // }
+    
+    // if (updateMethod === "append"){
+    //     await sails.sendNativeQuery(`UPDATE articles SET content = $1 sources = $2 WHERE articlename = $3`, [appendedContent, appendedSources, articleName])
+    //     const updatedArticle = await sails.sendNativeQuery(`SELECT * FROM articles WHERE articlename = $1`, [articleName])
 
-    if(!articleExists){
-      return res.status(400).send("No article found with that name. Please create the article first, or try again with an existing article name")
-    } 
+    //   if(updatedArticle){
+    //       return res.ok({success: true, article: updatedArticle.rows[0]})
+    //     } else {
+    //       return res.badRequest('Error updating')
+    //   }
+    // }
 
-    if(updateMethod === 'append'){
-      // CODE FOR APPENDING DATA
-    }else if (updateMethod === 'overwrite') {
-      // CODE FOR OVERWRITING ALL EXISTING DATA
-    } else {
-      return res.status(400).send("You must specify a valid value for updateMethod to complete this operation. Options are 'append' & 'overwrite'")
+    // OVERWRITE ARTICLE PROPERTIES
+    if(updateMethod === "overwrite") {
+        if (newContent && newSources) {
+            content = newContent
+            sources = newSources
+            await sails.sendNativeQuery(`UPDATE articles SET content = $1, sources = $2 WHERE articlename = $3`, [content, sources, articleName])
+            res.ok('Article updated')
+        } else if (newContent) {
+            content = newContent
+            await sails.sendNativeQuery(`UPDATE articles SET content = $1 WHERE articlename = $2`, [content, articleName])
+            res.ok('Article updated')
+        } else if (newSources) {
+            sources = newSources
+            await sails.sendNativeQuery(`UPDATE articles SET sources = $1 WHERE articlename = $2`, [sources, articleName])
+            res.ok('Article updated')
+        } else {
+          res.badRequest('You must provide data for the update request to proceed')
+        }
     }
-
-    const updatedArticle = await sails.sendNativeQuery(`SELECT * FROM articles WHERE articlename = $1`, [articleName])
-
-    // query to update existing article with returnedArticleProperties
-    // const articleUpdateQuery = `UPDATE articles
-    //   SET content = $1, sources = $2
-    //   WHERE articlename = $3`
-
-    // await sails.sendNativeQuery(articleUpdateQuery, [updatedContent, updatedSources, articleName])
-
-    return res.ok({"Success": true, "Article": updatedArticle})
   },
   // RATINGS - functions relating to the ratings feature for articles
-  getRating: async function(req, res){
-    const articlename = req.param('articlename').toLowerCase()
+  getRating: async function(req, res) {
+    const articlename = req.param("articlename").toLowerCase();
     const query = `SELECT articlename, rating FROM articles WHERE articlename = $1`;
-    const result = await sails.sendNativeQuery(query, [articlename])
-    const data = result.rows[0]
+    const result = await sails.sendNativeQuery(query, [articlename]);
+    const data = result.rows[0];
 
-    return res.ok(data)
+    return res.ok(data);
   },
-  postRating: async function(req, res){
-    const articlename = req.param('articlename').toLowerCase()
-    const voteDirection = req.param('voteDirection')
+  postRating: async function(req, res) {
+    const articlename = req.param("articlename").toLowerCase();
+    const voteDirection = req.param("voteDirection");
 
-    const ratingQuery = `SELECT rating FROM articles WHERE articlename = $1`
-    const updateQuery = `UPDATE articles SET rating = $1 WHERE articlename = $2`
-    const ratingResult = await sails.sendNativeQuery(ratingQuery, [articlename])
-  
-    let ratingValue = ratingResult.rows[0].rating
+    const ratingQuery = `SELECT rating FROM articles WHERE articlename = $1`;
+    const updateQuery = `UPDATE articles SET rating = $1 WHERE articlename = $2`;
+    const ratingResult = await sails.sendNativeQuery(ratingQuery, [
+      articlename
+    ]);
 
-    if(voteDirection === 'upvote'){
-      ratingValue = ratingValue + 1
-    } else if (voteDirection === 'downvote'){
-      ratingValue = ratingValue - 1
+    let ratingValue = ratingResult.rows[0].rating;
+
+    if (voteDirection === "upvote") {
+      ratingValue = ratingValue + 1;
+    } else if (voteDirection === "downvote") {
+      ratingValue = ratingValue - 1;
     }
 
-    await sails.sendNativeQuery(updateQuery, [ratingValue, articlename])
+    await sails.sendNativeQuery(updateQuery, [ratingValue, articlename]);
 
-    return res.ok(`Article rating updated`)
+    return res.ok(`Article rating updated`);
   }
 };
-
